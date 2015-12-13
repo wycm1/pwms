2b9cb1f0bb79dd1f0f3c1f9a1c82b462e1332a55
xiaob <xiaob@192.168.1.101>
2015-12-13 10:27:44 +0800

    add new java file

---
+++ pwms/src/com/pwms/controller/AdminNoticeController.java
@@ -0,0 +1,30 @@
+package com.pwms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/notice")
+public class AdminNoticeController extends BaseController {
+	//查看所有的文章列表
+	@RequestMapping("/all")
+	public String getAll(Model model){
+		return null;
+	}
+	//以表单的形式显示理论文章，便于用户修改文章
+	@RequestMapping("/modifyview")
+	public String modifyview(int id, Model model){
+		return null;
+	}
+	//根据id来修改文章内容
+	@RequestMapping("/modify")
+	public String modify(int id, Model model){
+		return null;
+	}
+	//删除文章
+	@RequestMapping("/delete")
+	public String delete(int id, Model model){
+		return null;
+	}
+}
