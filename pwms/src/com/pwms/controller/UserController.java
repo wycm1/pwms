ca63e974d3b55441d9f755bfcbd38e8d36892527
Administrator <Administrator@2NG7N5Z5JNXJCAP>
2015-12-15 11:50:06 +0800

    modify processRecordMapper.xml

--- pwms/src/com/pwms/controller/UserController.java
+++ pwms/src/com/pwms/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pwms.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -16,14 +17,14 @@ public class UserController extends BaseController{
     @Resource
     private IUserService userService;
 
-//    @RequestMapping("/showUser")
-//    public String toIndex(HttpServletRequest request, Model model) {
-//    	System.out.println("执行过来了");
-//        int userId = Integer.parseInt(request.getParameter("id"));
-//        User user = this.userService.getUserById(userId);
-//        model.addAttribute("user", user);
-//        return "hello";
-//    }
+    @RequestMapping("/showUser")
+    public String toIndex(HttpServletRequest request, Model model) {
+    	System.out.println("执行过来了");
+        int userId = Integer.parseInt(request.getParameter("id"));
+        User user = this.userService.getUserById(userId);
+        model.addAttribute("user", user);
+        return "hello";
+    }
     //登陆
     @RequestMapping("/login")
     public String login(HttpSession session, User user, Model model) {
