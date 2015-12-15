f322717e3c0043bd3481431101fab5a293d0da6c
xiaob <xiaob@192.168.1.101>
2015-12-15 23:38:29 +0800

    add new service

---
+++ pwms/src/com/pwms/service/INoticeService.java
@@ -0,0 +1,30 @@
+package com.pwms.service;
+
+import java.util.List;
+
+import com.pwms.pojo.NoticeTheroyContruction;
+
+public interface INoticeService {
+	/**
+	 * 保存对象
+	 * @param notice
+	 */
+	public void save(NoticeTheroyContruction notice);
+	/**
+	 * 通过id来获取对象
+	 * @param id
+	 * @return
+	 */
+	public NoticeTheroyContruction getNoticeById(Integer id);
+	/**
+	 * 通过类型来查找对象
+	 * @param type
+	 * @return
+	 */
+	public List<NoticeTheroyContruction> getNoticeByType(Integer type);
+	
+	public void modifyNotice(NoticeTheroyContruction notice);
+	
+	public void deleteNotice(NoticeTheroyContruction notice);
+	
+}
