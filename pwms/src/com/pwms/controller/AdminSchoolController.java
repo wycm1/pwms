185f5d6b9470a2dcc0c4c841c2d6a38b504c3b5f
xiaob <xiaob@192.168.1.101>
2016-01-02 15:31:06 +0800

    add

--- pwms/src/com/pwms/controller/AdminSchoolController.java
+++ pwms/src/com/pwms/controller/AdminSchoolController.java
@@ -4,16 +4,20 @@ import java.io.File;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pwms.pojo.CoureseGroup;
 import com.pwms.pojo.Course;
 import com.pwms.pojo.Exam;
 import com.pwms.pojo.ExamQuestion;
+import com.pwms.service.ICourseGroupService;
 import com.pwms.service.ICourseService;
+import com.pwms.service.IExamQuestionService;
 import com.pwms.service.IExamService;
 
 //党校管理
@@ -25,6 +29,10 @@ public class AdminSchoolController extends BaseController {
 	private ICourseService courseService;
 	@Resource
 	private IExamService examService;
+	@Resource
+	private IExamQuestionService questionService;
+	@Resource
+	private ICourseGroupService groupService;
 	//查看课程
 	@RequestMapping("/showcourse")
 	public String showCourse(Model model){
@@ -56,23 +64,36 @@ public class AdminSchoolController extends BaseController {
 	//查看课程组
 	@RequestMapping("/showgroup")
 	public String showGroup(Model model){
+		List<CoureseGroup> groupList = this.groupService.getCourseGroupList();
+		model.addAttribute("groupList", groupList);
 		return null;
 	}
 	//查看课程组里所有的课程
-	@RequestMapping("/group")
-	public String group(int groupid, Model model){
-//		this.courseService.ge
+	@RequestMapping("/group/{id}")
+	public String group(@PathVariable int id, Model model){
+		List<Course> courseList = this.courseService.getCourseByGroupId(id);
+		model.addAttribute("courseList", courseList);
 		return null;
 	}
 	//添加课程到课程组
 	@RequestMapping("/group/addCourse")
-	public String group(int groupid, int courseid, Model model){
-		return null;
+	public String group(HttpServletRequest request, int groupid, int courseid, Model model){
+		Course course = this.courseService.getCourse(courseid);
+		CoureseGroup group = this.groupService.getCourseGroupById(groupid);
+		if(group!=null&&course!=null){
+			course.setGroupId(group.getId());
+			this.courseService.save(course);
+			return publishmsg(request, "增加成功", null );
+		}else{
+		    return publishmsg(request, "修改失败", null );
+		}
 	}
 	//考试题目添加
 	@RequestMapping("/exam/addquestion")
-	public String addQuestion(ExamQuestion question, int examid, Model model){
-		return null;
+	public String addQuestion(HttpServletRequest request, ExamQuestion question, int examid, Model model){
+		Exam exam = this.examService.getExam(examid);
+		this.questionService.saveQuestion(question);
+		return publishmsg(request, "修改失败", null );
 	}
 	//考试题目导入
 	@RequestMapping("/exam/importxls")
